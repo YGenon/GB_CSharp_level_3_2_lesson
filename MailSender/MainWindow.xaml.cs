092a5a3eace8616e046e4c9e0d9f2ff6bc117b04
YGenon <genonster@gmail.com>
2020-03-24 15:38:12 +0300

    Старт проекта (сделаны 1 и 5 пункты)

---
+++ MailSender/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace MailSender
+{
+	public partial class MainWindow : Window
+	{
+		private EmailSendServiceClass _emailSender;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void TabSwitcherControl_OnBack(object sender, RoutedEventArgs e)
+		{
+			if (MainTabControl.SelectedIndex == 0) return;
+			MainTabControl.SelectedIndex--;
+		}
+
+		private void TabSwitcherControl_OnForward(object sender, RoutedEventArgs e)
+		{
+			if (MainTabControl.SelectedIndex == MainTabControl.Items.Count - 1) return;
+			MainTabControl.SelectedIndex++;
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			KeyValuePair<string, string> item = (KeyValuePair<string, string>) cbSenderSelect.SelectionBoxItem;
+			//MessageBox.Show(item.Value);
+			string strLogin = cbSenderSelect.Text;
+			string strPassword = cbSenderSelect.SelectedValue.ToString();
+			if (string.IsNullOrEmpty(strLogin))
+			{
+				MessageBox.Show("Выберите отправителя");
+				return;
+			}
+			if (string.IsNullOrEmpty(strPassword))
+			{
+				MessageBox.Show("Укажите пароль отправителя");
+				return;
+			}
+
+			KeyValuePair<string, int> item2 = (KeyValuePair<string, int>)cbServerSelect.SelectionBoxItem;
+			string strSmtp = item2.Key.ToString();
+			int strPort = Convert.ToInt32(item2.Value);
+			// проверяем что правильно передали параметры SMTP Servera
+			MessageBox.Show(strSmtp + " : " + strPort);
+
+			_emailSender = new EmailSendServiceClass(strLogin, strPassword, strSmtp, strPort);
+			
+			//_emailSender.SendMails((IQueryable<Emails>)dgEmails.ItemsSource);
+
+		}
+	}
+}
---
+++ MailSender/Senders.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MailSender
+{
+	public static class Senders
+	{
+		public static Dictionary<string, string> SendersDictionary { get; } = new Dictionary<string, string>
+		{
+			{"qwe@ewq.ru", EncrypterDll.Encrypter.Deencrypt("123")},
+			{"qaz@zaq.ru", EncrypterDll.Encrypter.Deencrypt("3241")}
+		};
+
+		public static Dictionary<string, int> ServerDictionary { get; } = new Dictionary<string, int>
+		{
+			{"smpt.yandex.ru", 25 },
+			{"smtp.google.com", 35 }
+
+		};
+	}
+}
